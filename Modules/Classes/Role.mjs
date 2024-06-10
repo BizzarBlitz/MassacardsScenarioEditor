2e206e2971d85f7469d3e849255da9f9e8ec8bc0
BizzarBlitz <bizzarblitz@gmail.com>
2024-06-10 17:26:04 -0400

    Create Role class and add scenario name

---
+++ Modules/Classes/Role.mjs
@@ -0,0 +1,241 @@
+// @ts-check
+
+// Modules
+
+import assert from "../assert.mjs"
+import fitText from "../fitText.mjs"
+import randomElement from "../randomElement.mjs"
+import removeItem from "../removeItem.mjs"
+
+// Constants
+
+const ALIGNMENTS = (await fetch("./Modules/alignments.json").then((response) => (response.json()))).alignments
+const ROLES = await fetch("./Modules/roles.json").then((response) => (response.json()))
+
+const UNSORTED_ROLES = (() => {
+	let unsortedRoles = []
+
+	for (const [_, roles] of Object.entries(ROLES)) {
+		unsortedRoles = unsortedRoles.concat(roles)
+	}
+
+	return unsortedRoles
+})()
+
+const RoleNode = assert(document.getElementById("role-template")).content.getElementById("role")
+
+
+
+export default class Role {
+	static AddedRoles = []
+
+	#Locked
+	Locked
+
+	static getAlignmentFromRoleName(roleName) {
+		for (const [alignmentName, alignmentRoles] of Object.entries(ROLES)) {
+			if (alignmentRoles.find((element) => (element === roleName))) return alignmentName
+		}
+
+		console.warn("Could not get alignment of role:", roleName)
+	}
+
+	static getRandomRoleName(alignment, randomizationSettings) {
+		randomizationSettings ??= {}
+		let rolePool = alignment === "Unknown" ? UNSORTED_ROLES : ROLES[alignment]
+
+		if (!randomizationSettings.AllowDuplicates) {
+			const newRolePool = [...rolePool] // Duplicates rolePool for mutation
+			let rolesToBeRandomized = 0
+
+			for (const existingRole of Role.AddedRoles) {
+				if (existingRole === randomizationSettings.Role) continue
+
+				const wasRemoved = removeItem(newRolePool, existingRole.Name)
+				if (wasRemoved && !existingRole.Locked) {
+					rolesToBeRandomized++
+				}
+			}
+
+			if (rolesToBeRandomized < rolePool.length) rolePool = newRolePool
+		}
+
+		return randomElement(rolePool)
+	}
+
+	constructor(name, alignment) {
+		const thisRole = this
+		const newRoleNode = RoleNode.cloneNode(true)
+
+		this.Name = name
+		this.Alignment = alignment
+		this.AlignmentIndex = ALIGNMENTS.indexOf(alignment)
+		this.#Locked = false
+
+		this.Elements = {
+			Role: newRoleNode,
+			RoleName: newRoleNode.getElementsByClassName("role-name")[0],
+			RoleImage: newRoleNode.getElementsByClassName("role-image")[0],
+
+			RemoveRoleButton: newRoleNode.getElementsByClassName("remove-role")[0],
+			DuplicateRoleButton: newRoleNode.getElementsByClassName("duplicate-role")[0],
+
+			RandomizeButton: newRoleNode.getElementsByClassName("role-button randomize")[0],
+			LockButton: newRoleNode.getElementsByClassName("role-button lock")[0],
+
+			AlignmentName: newRoleNode.getElementsByClassName("alignment-name")[0],
+			AlignmentImage: newRoleNode.getElementsByClassName("alignment-image")[0],
+			PreviousAlignmentButton: newRoleNode.getElementsByClassName("arrow left-arrow")[0],
+			NextAlignmentButton: newRoleNode.getElementsByClassName("arrow right-arrow")[0],
+		}
+
+		Object.defineProperty(this, "Locked", {
+			set(locked) {
+				this.#Locked = locked
+				thisRole.Elements.LockButton.dataset.enabled = locked ? "true" : "false"
+			},
+
+			get() {
+				return this.#Locked
+			}
+		})
+	}
+
+	updateRoleName(name, wasNameValid) {
+		this.Elements.RoleName.value = wasNameValid ? name : "Invalid role"
+		this.Elements.RoleImage.src = `../../Files/Roles/${name}.png`
+		this.Elements.RoleImage.alt = name
+
+		this.Name = name
+
+		fitText(this.Elements.RoleName)
+	}
+
+	parseRoleName() {
+		const thisRole = this
+		let roleName = this.Elements.RoleName.value
+		let validRoleName = true
+
+		fetch(`../../Files/Roles/${roleName}.png`).then(function(response) {
+			if (response.status === 404) {
+				// Invalid role name
+				console.warn("Invalid role name:", roleName)
+				roleName = "Placeholder"
+				validRoleName = false
+			}
+
+			thisRole.updateRoleName(roleName, validRoleName)
+		})
+	}
+
+	updateRoleAlignment(alignment, wasAlignmentValid) {
+		let alignmentNameText
+
+		this.Elements.AlignmentName.value = wasAlignmentValid ? alignment : "Invalid alignment"
+
+		setTimeout(() => {
+			if (this.Elements.AlignmentName.value !== alignmentNameText) return
+
+			this.Elements.AlignmentName.value = alignment
+			fitText(this.Elements.AlignmentName)
+		}, 1000)
+		
+		this.Elements.AlignmentImage.src = `../../Files/Alignments/${alignment}.png`
+		this.Elements.AlignmentImage.alt = alignment
+
+		this.Alignment = alignment
+		this.AlignmentIndex = ALIGNMENTS.indexOf(alignment)
+
+		alignmentNameText = this.Elements.AlignmentName.value
+
+		fitText(this.Elements.AlignmentName)
+	}
+
+	parseAlignmentName() {
+		const thisRole = this
+		let alignmentName = this.Elements.AlignmentName.value
+		let validAlignmentName = true
+
+		fetch(`../../Files/Alignments/${alignmentName}.png`).then(function (response) {
+			if (response.status === 404) {
+				// Invalid alignment name
+				console.warn("Invalid alignment name:", alignmentName)
+				alignmentName = "Unknown"
+				validAlignmentName = false
+			}
+
+			thisRole.updateRoleAlignment(alignmentName, validAlignmentName)
+		})
+	}
+
+	setToAny(alignment) {
+		alignment ??= this.Alignment
+
+		this.Elements.RoleName.value = "Any " + (alignment === "Unknown" ? "Role" : alignment)
+		this.Elements.RoleImage.src = `../../Files/Alignments/${alignment}.png`
+
+		fitText(this.Elements.RoleName)
+	}
+
+	incrementAlignmentIndex(increment) {
+		this.AlignmentIndex = (this.AlignmentIndex + increment) % (ALIGNMENTS.length)
+		if (this.AlignmentIndex === -1) this.AlignmentIndex = ALIGNMENTS.length - 1 // Because modulus doesn't do what I want it to with negatives
+
+		this.updateRoleAlignment(ALIGNMENTS[this.AlignmentIndex], true)
+	}
+
+	duplicate() {
+		const newRole = new Role(this.Name, this.Alignment)
+
+		newRole.Locked = this.Locked
+		newRole.add(assert(this.Elements.Container, "Attempt to duplicate role outside container"))
+	}
+
+	randomize(bypassLock, randomizationSettings) {
+		if (this.Locked && !bypassLock) return
+
+		this.updateRoleName(Role.getRandomRoleName(this.Alignment, randomizationSettings), true)
+	}
+
+	add(container) {
+		const thisRole = this
+
+		this.Elements.RoleName.addEventListener("keydown", function(event) {
+			if (event.key !== "Enter") return;
+			thisRole.parseRoleName()
+		})
+
+		this.Elements.AlignmentName.addEventListener("keydown", function(event) {
+			if (event.key !== "Enter") return;
+			thisRole.parseAlignmentName()
+		})
+
+		this.Elements.AlignmentImage.onclick = () => (thisRole.setToAny(thisRole.Alignment))
+
+		this.Elements.PreviousAlignmentButton.onclick = () => (thisRole.incrementAlignmentIndex(-1))
+		this.Elements.NextAlignmentButton.onclick = () => (thisRole.incrementAlignmentIndex(1))
+
+		this.Elements.RandomizeButton.onclick = () => (thisRole.randomize(true))
+		this.Elements.LockButton.onclick = () => (thisRole.Locked = !thisRole.Locked)
+
+		this.Elements.RemoveRoleButton.onclick = () => (thisRole.remove())
+		this.Elements.DuplicateRoleButton.onclick = () => (thisRole.duplicate())
+
+		this.Elements.RoleName.onchange = () => (fitText(thisRole.Elements.RoleName))
+		this.Elements.RoleName.onkeypress = () => (fitText(thisRole.Elements.RoleName))
+		this.Elements.AlignmentName.onchange = () => (fitText(thisRole.Elements.AlignmentName))
+		this.Elements.AlignmentName.onkeypress = () => (fitText(thisRole.Elements.AlignmentName))
+
+		this.updateRoleName(this.Name, true)
+		this.updateRoleAlignment(this.Alignment, true)
+
+		this.Elements.Container = container
+		container.appendChild(this.Elements.Role)
+		Role.AddedRoles.push(this)
+	}
+
+	remove() {
+		this.Elements.Role.remove()
+		removeItem(Role.AddedRoles, this)
+	}
+}
--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 // Would really like to use actual TS for this... oh well
 
 // Modules
+import Role from "./Modules/Classes/Role.mjs"
+
 import assert from "./Modules/assert.mjs"
+import fitText from "./Modules/fitText.mjs"
 import randomElement from "./Modules/randomElement.mjs"
 import removeItem from "./Modules/removeItem.mjs"
 
@@ -27,284 +30,47 @@ const UrlSearchParams = new URLSearchParams(window.location.search)
 
 const randomizeScenarioButton = assert(document.getElementById("randomize-scenario"))
 const roleContainer = assert(document.getElementById("role-container"))
-const role = assert(document.getElementById("role-template")).content.getElementById("role")
 const createRoleButton = assert(document.getElementById("insert-role"))
 
 const randomizationSettings = {
 	AllowDuplicates: false,
 }
-const currentRoles = []
 
 
 // Functions
 
-// From Stack Overflow (I'm pro dev I know): https://stackoverflow.com/a/56099826
-
-function isOverflown(element) {
-	return element.scrollWidth > element.clientWidth
-}
-
-async function delayedIsOverflown(element) {
-	return await new Promise((resolve) => {
-		requestAnimationFrame(() =>
-			requestAnimationFrame(() => (
-				resolve(isOverflown(element))
-			))
-		)
-	})
-}
-
-async function shrinkTextOnOverflow(element) {
-	if (element.dataset.isShrinking === true) return
-	element.dataset.isShrinking = true
-
-	const initialValue = element.value
-
-	// await delayedIsOverflown(element)
-
-	// let currentFontSize = Number((element.dataset.fontSize || window.getComputedStyle(element).fontSize).slice(0, -2)) // Trim "px" off
-	let currentFontSize = 16
-
-	function shrink() {
-		currentFontSize -= 0.5
-		element.style.fontSize = currentFontSize + "px"
-	}
-
-	element.style.fontSize = currentFontSize + "px"
-
-	while (isOverflown(element) && initialValue === element.value) shrink()
-
-	while (initialValue === element.value && await delayedIsOverflown(element)) shrink()
-
-	element.dataset.fontSize = currentFontSize + 'px'
-	element.dataset.isShrinking = false
-}
-
-function getRoleAlignment(roleName) {
-	for (const [alignmentName, alignmentRoles] of Object.entries(ROLES)) {
-		if (alignmentRoles.find((element) => (element === roleName))) {
-			return alignmentName
-		}
-	}
-
-	console.warn("Could not get alignment of role:", roleName)
-}
-
-async function createRole(name, alignment) {
-	const newRole = role.cloneNode(true)
-
-	const roleStruct = {
-		Role: newRole,
-		RoleName: newRole.getElementsByClassName("role-name")[0],
-		RoleImage: newRole.getElementsByClassName("role-image")[0],
-
-		RemoveRoleButton: newRole.getElementsByClassName("remove-role")[0],
-		DuplicateRoleButton: newRole.getElementsByClassName("duplicate-role")[0],
-
-		RandomizeButton: newRole.getElementsByClassName("role-button randomize")[0],
-		LockButton: newRole.getElementsByClassName("role-button lock")[0],
-
-		AlignmentName: newRole.getElementsByClassName("alignment-name")[0],
-		AlignmentImage: newRole.getElementsByClassName("alignment-image")[0],
-		PreviousAlignmentButton: newRole.getElementsByClassName("arrow left-arrow")[0],
-		NextAlignmentButton: newRole.getElementsByClassName("arrow right-arrow")[0],
-
-		Name: name,
-		Alignment: alignment,
-		AlignmentIndex: ALIGNMENTS.indexOf(alignment),
-		Locked: false,
-	}
-
-	roleStruct.RoleName.value = name
-	roleStruct.RoleImage.src = `Files/Roles/${name}.png`
-	roleStruct.RoleImage.alt = name
-
-	roleStruct.AlignmentName.value = alignment
-	roleStruct.AlignmentImage.src = `Files/Alignments/${alignment}.png`
-
-	return roleStruct
-}
-
-// Assumes roleName is valid
-function updateRoleName(role, roleName, wasNameValid) {
-	role.RoleName.value = wasNameValid ? roleName : "Invalid role"
-	role.RoleImage.src = `Files/Roles/${roleName}.png`
-	role.RoleImage.alt = roleName
-
-	role.Name = roleName
-
-	shrinkTextOnOverflow(role.RoleName)
-}
-
-// Assume alignment is valid
-function updateRoleAlignment(role, alignment, wasAlignmentValid) {
-	let alignmentNameText
-
-	role.AlignmentName.value = wasAlignmentValid ? alignment : "Invalid alignment"
-	setTimeout(() => {
-		if (role.AlignmentName.value !== alignmentNameText) return
-		role.AlignmentName.value = alignment; shrinkTextOnOverflow(role.AlignmentName)
-	}, 1000)
-	role.AlignmentImage.src = `Files/Alignments/${alignment}.png`
-	role.AlignmentImage.alt = alignment
-
-	role.Alignment = alignment
-	role.AlignmentIndex = ALIGNMENTS.indexOf(alignment)
-
-	alignmentNameText = role.AlignmentName.value
-
-	shrinkTextOnOverflow(role.AlignmentName)
-}
-
-function getRandomRoleName(alignment) {
-	let rolePool = alignment === "Unknown" ? UNSORTED_ROLES : ROLES[alignment]
-
-	if (!randomizationSettings.AllowDuplicates) {
-		const newRolePool = [...rolePool] // Clones rolePool for mutation
-		let rolesToBeRandomized = 0
-
-		for (const existingRole of currentRoles) {
-			const wasRemoved = removeItem(newRolePool, existingRole.Name)
-
-			if (wasRemoved && !existingRole.Locked) {
-				rolesToBeRandomized++
-			}
-		}
-
-		if (rolesToBeRandomized < rolePool.length) rolePool = newRolePool
-	}
-
-	return randomElement(rolePool)
-}
-
-function randomizeRole(role, bypassLock) {
-	if (role.Locked && !bypassLock) return
-	
-	updateRoleName(role, getRandomRoleName(role.Alignment), true)
-}
-
-function randomizeScenario() {
-	for (const role of currentRoles) {
-		randomizeRole(role)
+randomizeScenarioButton.onclick = function() {
+	for (const role of Role.AddedRoles) {
+		role.randomize()
 	}
 }
-
-function addRole(role) {
-	role.RoleName.addEventListener("keydown", function(event) {
-		if (event.key !== "Enter") return;
-
-		let roleName = role.RoleName.value
-		let validRoleName = true
-
-		fetch(`Files/Roles/${roleName}.png`).then(function(response) {
-			if (response.status === 404) {
-				// Invalid role name
-				console.warn("Invalid role name:", roleName)
-				roleName = "Placeholder"
-				validRoleName = false
-			}
-
-			updateRoleName(role, roleName, validRoleName)
-		})
-	})
-
-	role.AlignmentName.addEventListener("keydown", function(event) {
-		if (event.key !== "Enter") return;
-
-		let alignmentName = role.AlignmentName.value
-		let validAlignmentName = true
-
-		fetch(`Files/Alignments/${alignmentName}.png`).then(function(response) {
-			if (response.status === 404) {
-				// Invalid alignment name
-				console.warn("Invalid alignment name:", alignmentName)
-				alignmentName = "Unknown"
-				validAlignmentName = false
-			}
-
-			updateRoleAlignment(role, alignmentName, validAlignmentName)
-		})
-	})
-
-	role.AlignmentImage.onclick = function() {
-		role.RoleName.value = "Any " + (role.Alignment === "Unknown" ? "Role" : role.Alignment)
-		role.RoleImage.src = `Files/Alignments/${role.Alignment}.png`
-
-		shrinkTextOnOverflow(role.RoleName)
-	}
-
-	role.PreviousAlignmentButton.onclick = function() {
-		role.AlignmentIndex = ((role.AlignmentIndex - 1) % (ALIGNMENTS.length))
-		if (role.AlignmentIndex === -1) role.AlignmentIndex = ALIGNMENTS.length - 1 // Because modulus doesn't do what I want it to with negatives
-
-		updateRoleAlignment(role, ALIGNMENTS[role.AlignmentIndex], true)
-	}
-
-	role.NextAlignmentButton.onclick = function() {
-		role.AlignmentIndex = (role.AlignmentIndex + 1) % (ALIGNMENTS.length)
-		updateRoleAlignment(role, ALIGNMENTS[role.AlignmentIndex], true)
-	}
-
-	role.RandomizeButton.onclick = function() {
-		randomizeRole(role, true)
-	}
-
-	role.LockButton.onclick = function() {
-		role.Locked = !role.Locked
-		role.LockButton.dataset.enabled = role.LockButton.dataset.enabled === "true" ? "false" : "true"
-	}
-
-	role.RemoveRoleButton.onclick = function() {
-		role.Role.remove()
-		removeItem(currentRoles, role)
-	}
-
-	role.DuplicateRoleButton.onclick = async function() {
-		addRole(await createRole(role.Name, role.Alignment))
-	}
-
-	role.RoleName.onchange = () => (shrinkTextOnOverflow(role.RoleName))
-	role.RoleName.onkeypress = () => (shrinkTextOnOverflow(role.RoleName))
-	role.AlignmentName.onchange = () => (shrinkTextOnOverflow(role.AlignmentName))
-	role.AlignmentName.onkeypress = () => (shrinkTextOnOverflow(role.AlignmentName))
-	
-	roleContainer.appendChild(role.Role)
-	shrinkTextOnOverflow(role.AlignmentName)
-
-	currentRoles.push(role)
-}
-
-randomizeScenarioButton.onclick = randomizeScenario
 
 createRoleButton.onclick = async function() {
-	if (!currentRoles.find((role) => (role.Name === "Killer" || role.Name === "Massacre"))) addRole(await createRole("Killer", "Murderous"))
-	else if (!currentRoles.find((role) => (role.Name === "Private Eye" || role.Name === "Hero"))) addRole(await createRole("Private Eye", "Dark Innocent"))
-	else if (!currentRoles.find((role) => (role.Name === "Witness"))) addRole(await createRole("Witness", "Innocent"))
-	else addRole(await createRole("Bystander", "Innocent"))
+	if (!Role.AddedRoles.find((role) => (role.Name === "Killer" || role.Name === "Massacre"))) new Role("Killer", "Murderous").add(roleContainer)
+	else if (!Role.AddedRoles.find((role) => (role.Name === "Private Eye" || role.Name === "Hero"))) new Role("Private Eye", "Dark Innocent").add(roleContainer)
+	else if (!Role.AddedRoles.find((role) => (role.Name === "Witness"))) new Role("Witness", "Innocent").add(roleContainer)
+	else new Role("Bystander", "Innocent").add(roleContainer)
 }
 
-async function createDefaultRoles() {
-	const killer = await createRole("Killer", "Murderous")
-	const privateEye = await createRole("Private Eye", "Dark Innocent")
-	const witness = await createRole("Witness", "Innocent")
+function createDefaultRoles() {
+	const killer = new Role("Killer", "Murderous")
+	const privateEye = new Role("Private Eye", "Dark Innocent")
+	const witness = new Role("Witness", "Innocent")
 
 	killer.Locked = true
-	killer.LockButton.dataset.enabled = "true"
 	privateEye.Locked = true
-	privateEye.LockButton.dataset.enabled = "true"
 	witness.Locked = true
-	witness.LockButton.dataset.enabled = "true"
 
-	addRole(killer)
-	addRole(privateEye)
-	addRole(witness)
+	killer.add(roleContainer)
+	privateEye.add(roleContainer)
+	witness.add(roleContainer)
 }
 
-async function createRolesFromUriComponent(rolesString) {
+function createRolesFromUriComponent(rolesString) {
 	const roles = rolesString.split("_")
 
-	for (const role of roles) {
-		addRole(await createRole(role, getRoleAlignment(role)))
+	for (const roleName of roles) {
+		new Role(roleName, Role.getAlignmentFromRoleName(roleName)).add(roleContainer)
 	}
 }
 
